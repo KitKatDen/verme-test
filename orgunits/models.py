b8d6d26f47596dd0e02c88ae5c73e14b296bbf29
Den <maldenser@yandex.ru>
2020-10-30 16:55:09 +0300

    Done: pytest orgunits/tests/models/

--- orgunits/models.py
+++ orgunits/models.py
@@ -14,16 +14,38 @@ class OrganizationQuerySet(models.QuerySet):
 
         :type root_org_id: int
         """
-        return self.filter()
+        queryset = self
+        for organization in queryset:
+            if organization.id == root_org_id:
+                continue
+            elif organization.parent_id == root_org_id:
+                continue
+            elif organization.parent and organization.parent.parent_id == root_org_id:
+                continue
+            else:
+                queryset = queryset.exclude(id=organization.id)
+        return queryset
 
     def tree_upwards(self, child_org_id):
         """
         Возвращает корневую организацию с запрашиваемым child_org_id и всех её родителей любого уровня вложенности
         TODO: Написать фильтр с помощью ORM или RawSQL запроса или функций Python
-
         :type child_org_id: int
         """
-        return self.filter()
+        queryset = self
+        child_org = queryset.filter(id=child_org_id).first()
+        print(child_org)
+        for organization in queryset:
+            if organization.id == child_org_id:
+                continue
+            elif organization.id == child_org.parent_id:
+                continue
+            elif child_org.parent and organization.id == child_org.parent.parent_id:
+                continue
+            else:
+                queryset = queryset.exclude(id=organization.id)
+
+        return queryset
 
 
 class Organization(models.Model):
@@ -46,14 +68,21 @@ class Organization(models.Model):
         """
         Возвращает всех родителей любого уровня вложенности
         TODO: Написать метод, используя ORM и .tree_upwards()
-
         :rtype: django.db.models.QuerySet
         """
+
+        parents_without_self = Organization.objects.tree_upwards(self.id).exclude(id=self.id)
+        return parents_without_self
 
     def children(self):
         """
         Возвращает всех детей любого уровня вложенности
         TODO: Написать метод, используя ORM и .tree_downwards()
-
         :rtype: django.db.models.QuerySet
         """
+
+        children_without_self = Organization.objects.tree_downwards(self.id).exclude(id=self.id)
+        return children_without_self
+
+    def __str__(self):
+        return self.name
